d7382721c23296d683eb25925764df428d3ef2d0
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-06-16 20:51:32 -0400

    pushing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # passwordgen
+client wanted a random password generator
+so when client press on the Generated Password button a prompt will showing asking how many characters they would like (8 to 128)
--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = "!@#$%^&*_-+=";
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var symbols = ["!","@","#","$","%","^","&","*","_","-","+", "=" ]
 
 var confirmLegth = "";
 var confirmUpperCase;
@@ -15,57 +15,66 @@ var confrimSymbols;
 function generatePassword() {
   var confirmLegth = (prompt(
     "How many characters would you like in your password? choose between 8 and 128"
-  ));
-  
-  if (confirmLegth < 8 || confirmLegth > 128)
-  {alert ("need to choose between 8 and 128 charters")};
-
-
-
-  var confirmLowerCase = confirm(
-    "click ok if you would like to include lowercase charters"
-  );
-  var confirmUpperCase = confirm(
-    " Click ok if you would like to include uppercase charters"
-  );
-  var confirmNumbers = confirm(
-    " click ok if you would like to include numbers"
-  );
-  var confrimSymbols = confirm("click ok if you would like to include symbols");
-
-
-  var passwordCharacters = []
-  if (confirmLowerCase){
-  passwordCharacters = passwordCharacters.concat(alphaLower)}
-
-  if (confirmUpperCase) {
-    passwordCharacters = passwordCharacters.concat(alphaUpper)
-  }
-  if (confirmNumbers){
-    passwordCharacters = passwordCharacters.concat(numbers)
-  }
-  if (confrimSymbols){
-    passwordCharacters = passwordCharacters.concat(symbols)
-  }
-
-  var randompassword= ""
-  
-  for ( var i =0; i < confirmLegth; i++ ){
-    var randompassword = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)] ;
-    console.log(randompassword)
-    return randompassword;
-  }
-}
-  
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    ));
+    
+    if (confirmLegth < 8|| confirmLegth > 128 ){alert("need to choose between 8 and 128 charters")};
+    
+    
+    
+    var confirmLowerCase = confirm(
+      "click ok if you would like to include lowercase charters"
+      );
+      var confirmUpperCase = confirm(
+        " Click ok if you would like to include uppercase charters"
+        );
+        var confirmNumbers = confirm(
+          " click ok if you would like to include numbers"
+          );
+          var confrimSymbols = confirm("click ok if you would like to include symbols");
+          if  (confirmLowerCase == false && confirmUpperCase == false && confirmNumbers == false && confrimSymbols
+          == false) {alert("one need to be chosen") }
+          
+          
+          var passwordCharacters = []
+          if (confirmLowerCase) {
+            passwordCharacters = passwordCharacters.concat(alphaLower)
+            console.log(passwordCharacters)
+          }
+          
+          if (confirmUpperCase) {
+            passwordCharacters = passwordCharacters.concat(alphaUpper)
+            console.log(passwordCharacters)
+          }
+          if (confirmNumbers) {
+            passwordCharacters = passwordCharacters.concat(numbers)
+            console.log(passwordCharacters)
+          }
+          if (confrimSymbols) {
+            passwordCharacters = passwordCharacters.concat(symbols)
+            console.log(passwordCharacters)
+          }
+          
+          
+          var randomPassword = ""
+          
+          for (var i = 0; i < confirmLegth; i++) {
+           var randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+            console.log(randomPassword)
+          }
+          return randomPassword;
+        }
+        
+        
+        
+        // Write password to the #password input
+        function writePassword() {
+          var password = generatePassword();
+          var passwordText = document.querySelector("#password");
+          
+          
+          passwordText.value = password;
+        }
+        
+        // Add event listener to generate button
+        generateBtn.addEventListener("click", writePassword);
+        
